79f81a36ce1e130d24fbd99d0bae701d3294c3ab
jyek <justinyek@gmail.com>
2014-03-08 17:07:16 -0800

    Bar charts

--- stock.js
+++ stock.js
@@ -43,19 +43,40 @@ var getDataForTicker = function(ticker){
   var todayStr = today.getFullYear + '-' + today.getMonth() + '-' + today.getDate();
   getStock({ stock: ticker, startDate: '2014-01-01', endDate: todayStr }, 'historicaldata', function(err, data) {
     var ticks = data.quote;
-    console.log(ticks);
+    plotColumnGraph(ticks);
   });
 };
 
 // http://bost.ocks.org/mike/bar/3/
 var plotColumnGraph = function(data){
   var width = 960,
-      height = 500;
+      height = 400;
 
+  console.log(data);
 
+  var y = d3.scale.linear().domain([1000,1300]).range([0, height]);
+
+  var chart = d3.select('.chart')
+    .attr('width', width)
+    .attr('height', height);
+
+  var barWidth = width / data.length;
+
+  var bar = chart.selectAll('g')
+    .data(data)
+    .enter().append('g')
+    .attr('transform', function(d, i){ return "translate(" + i * barWidth + ",0)"; });
+
+  bar.append('rect')
+    .attr('y', function(d){ return height - y(d.Close); })
+    .attr('height', function(d){ return y(d.Close); })
+    .attr('width', barWidth - 1);
 };
 
 $(document).ready(function(){
+  // initial ticker
+  var ticker = 'GOOG';
+
   // tests
   getStock({ stock: 'AAPL' }, 'quotes', function(err, data) {
     console.log('Test1: ', data);
@@ -65,10 +86,13 @@ $(document).ready(function(){
     console.log('Test2: ', data);
   });
 
+  // default
+  getDataForTicker(ticker);
+
   // get graph on submit
   $('#submit').click(function(){
     event.preventDefault();
-    var ticker = $("#ticker").val();
+    ticker = $("#ticker").val();
     getDataForTicker(ticker);
   });
 });
